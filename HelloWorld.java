4d6889f0bd1eb18a930029a7424edd3a3109b805
Kovács Tomaj <48093726+tomaj0606@users.noreply.github.com>
2023-08-16 21:03:01 +0200

    Update HelloWorld.java 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Made some changes"); 
+        System.out.println("Made some changes again"); 
     }
 }
